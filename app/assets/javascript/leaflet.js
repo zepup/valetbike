ab26b593cef408d481e94b92e9132e3b04a0ba20
i-m-foster <irenefoster64@gmail.com>
2022-04-09 16:35:37 -0400

    ajax coding notes

--- app/assets/javascript/leaflet.js
+++ app/assets/javascript/leaflet.js
@@ -1,7 +1,18 @@
 // Initializing map (set View contains coordinates of map)
 var map = L.map('map').setView([42.33, -72.64], 13);
 
-// Sets the map tile to the Open Street Map tile 
+// Sets the map tile to the Open Street Map tile
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+// $.ajax({
+//   type:"GET",
+//   url:"books/test",
+//   dataType:"json",
+//   data: {some_parameter: 'hello'},
+//   success:function(result){
+//     alert(result);
+//   }
+// })
