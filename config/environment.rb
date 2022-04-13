41b4e650edcbb3755fab4eb6246e04bb811305cb
Zephyr Wang <zephyrwang@Zephyrs-Mac.local>
2022-04-13 15:48:19 -0400

    email confirmation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'http://127.0.0.1:3000',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_195724) do
+ActiveRecord::Schema.define(version: 2022_04_13_010125) do
 
   create_table "bikes", force: :cascade do |t|
     t.integer "identifier"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 2022_04_05_195724) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
