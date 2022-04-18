921230297f783ab0ed2b991906ac367354ccd333
i-m-foster <irenefoster64@gmail.com>
2022-04-18 15:44:14 -0400

    allowing map to show up and commenting out code

--- app/assets/javascript/leaflet.js
+++ app/assets/javascript/leaflet.js
@@ -19,38 +19,38 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker = L.marker([42.33, -72.64]).addTo(map);
 
-// GET request
-$.ajax({
-    url: '/users',
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-        console.log(data);
-    },
-    error: function (error) {
-        console.log(`Error ${error}`);
-    }
-});
-
-// POST request
-$.ajax({
-    url: '/map',
-    type: "POST",
-    data: {
-        name: "Ipseeta",
-        id: 1
-    },
-    dataType: "json",
-    success: function (data) {
-        console.log(data);
-    },
-    error: function (error) {
-        console.log(`Error ${error}`);
-    }
-});
-
-
-
-for (station in stations) {
-  var marker = L.marker([s.lat, s.long]).addTo(map);
-}
+// // GET request
+// $.ajax({
+//     url: '/users',
+//     type: "GET",
+//     dataType: "json",
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error: function (error) {
+//         console.log(`Error ${error}`);
+//     }
+// });
+//
+// // POST request
+// $.ajax({
+//     url: '/map',
+//     type: "POST",
+//     data: {
+//         name: "Ipseeta",
+//         id: 1
+//     },
+//     dataType: "json",
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error: function (error) {
+//         console.log(`Error ${error}`);
+//     }
+// });
+//
+//
+//
+// for (station in stations) {
+//   var marker = L.marker([s.lat, s.long]).addTo(map);
+// }
