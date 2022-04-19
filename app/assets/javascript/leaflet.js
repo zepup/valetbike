9449fb00825c07b0cb987ccf79799ddaf8cd6ba6
i-m-foster <irenefoster64@gmail.com>
2022-04-19 16:17:45 -0400

    fixing rake task

--- app/assets/javascript/leaflet.js
+++ app/assets/javascript/leaflet.js
@@ -54,3 +54,53 @@ var marker = L.marker([42.33, -72.64]).addTo(map);
 // for (station in stations) {
 //   var marker = L.marker([s.lat, s.long]).addTo(map);
 // }
+
+
+
+// $.ajax({
+//   type:"GET",
+//   url:"books/test",
+//   dataType:"json",
+//   data: {some_parameter: 'hello'},
+//   success:function(result){
+//     alert(result);
+//   }
+//
+
+// var marker = L.marker([42.33, -72.64]).addTo(map);
+//
+// // // GET request
+// // $.ajax({
+//     url: '/users',
+//     type: "GET",
+//     dataType: "json",
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error: function (error) {
+//         console.log(`Error ${error}`);
+//     }
+// });
+//
+// // POST request
+// $.ajax({
+//     url: '/map',
+//     type: "POST",
+//     data: {
+//         name: "Ipseeta",
+//         id: 1
+//     },
+//     dataType: "json",
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error: function (error) {
+//         console.log(`Error ${error}`);
+//     }
+// });
+//
+//
+//
+// for (station in stations) {
+//   var marker = L.marker([s.lat, s.long]).addTo(map);
+// }
