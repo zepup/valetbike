aa20e2953fcd133b082afaa78f752c74a01bf707
Zephyr Wang <zephyrwang@Zephyrs-Mac.local>
2022-04-21 22:55:26 -0400

    modidy confirmations

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'http://127.0.0.1:3000',
   :address => 'smtp.sendgrid.net',
   :port => 587,
