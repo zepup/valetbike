4ba99fee18767ad9d107cdc9d70116e4fe5dbb97
zepup <79200388+zepup@users.noreply.github.com>
2022-04-24 20:27:09 -0400

    Update routes.rb
    solve sign out issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
   resources :memberships
   resources :products
   post "checkout/create", to: "checkout#create"
